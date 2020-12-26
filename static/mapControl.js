564a96dbe73829c629a9fac8f09eaf6117688fd1
ddword <desigilla@gmail.com>
2020-12-26 16:59:35 -0500

    fix add dynamically markers on map

--- static/mapControl.js
+++ static/mapControl.js
@@ -1,58 +1,43 @@
     // get points data from data-attribute
-    var container = document.querySelector("#map");
-    var data = container.getAttribute("data-points");
-    var points = JSON.parse(data);
-    console.log('Points', points);
-    var attribution = new ol.control.Attribution({
+    const container = document.querySelector("#map");
+    const data = container.getAttribute("data-points");
+    const points = JSON.parse(data);
+    const attribution = new ol.control.Attribution({
         collapsible: false
     });
-    var markerCenter = new ol.Feature({
-      geometry: new ol.geom.Point(
-        ol.proj.fromLonLat([-73.65269, 45.48858])
-      ),
-      type: 'icon'
-    });
-    var iconStyles = new ol.style.Style({
+    const iconStyles = new ol.style.Style({
         image: new ol.style.Icon({
           anchor: [0.5, 1],
           src: 'static/blue.png',
         })
     });
-    var markerLasalle = new ol.Feature({
-      geometry: new ol.geom.Point(
-        ol.proj.fromLonLat([-73.63, 45.43])
-      ),
-      type: 'icon'
-    });
-    var vectorSource = new ol.source.Vector();
-    /* example of the pattern:*/
-    // var coordinate = [lon, lat];
-    var markers = [];
-    var setMarker = function(lon, lat) {
+    const vectorSource = new ol.source.Vector();
+    const markers = [];
+    const setMarker = (longitude, latitude) => {
         return new ol.Feature({
             geometry: new ol.geom.Point(
-                ol.proj.fromLonLat([lon, lat])
+                ol.proj.fromLonLat([longitude, latitude])
             ),
             type: 'v-icon'
         });
-        // vectorSource.addFeature(marker);
     }
     points.forEach(function(point){
         point = Array.from(point);
-        console.log('Mark Points', point, Number(point[2]))
-        markers.push(setMarker(Number(point[2]),Number(point[3])))
+        /** @params longitude and latitude
+          * longitude = Number(point[3]) and latitude = Number(point[2])
+          */
+        const marker = setMarker(Number(point[3]),Number(point[2]))
+        markers.push(marker)
     })
-    console.log('Markers', markers)
-    console.log('Compare default markers addFeatures', [markerCenter, markerLasalle])
-    // vectorSource.addFeature(markers)
-    vectorSource.addFeatures([markerCenter, markerLasalle])
-    var vectorLayer = new ol.layer.Vector({
+    // console.log('Markers', markers)
+    vectorSource.addFeatures(Array.from(markers))
+    const vectorLayer = new ol.layer.Vector({
         source: vectorSource,
         style: iconStyles,
         updateWhileAnimating: true,
         updateWhileInteracting: true
     })
-    var map = new ol.Map({
+    const map = new ol.Map({
          target: 'map',
          view: new ol.View({
              center: ol.proj.fromLonLat([-73.7, 45.5]),
